c27fa559a71f21f00d003f8ab1004562bf18678d
senandtihiro <yulian_he@126.com>
2019-03-16 20:12:12 +0800

    修改功能实现

--- routes/prompt.py
+++ routes/prompt.py
@@ -21,9 +21,22 @@ def add():
     return redirect(url_for('.index'))
 
 
-
 @main.route('/delete/<string:title>')
 def delete(title):
     print('delete called')
     mongodb['Prompt'].remove({'title': title})
     return redirect(url_for('.index'))
+
+
+@main.route('/modify', methods=['POST'])
+def modify():
+    form = request.form.to_dict()
+    pid = form.get('id')
+    mongodb['Prompt'].update({"id": pid}, {"$set": form})
+    return redirect(url_for('.index'))
+
+
+@main.route('/edit/<id>')
+def edit(id):
+    _p = mongodb['Prompt'].find_one({'id': str(id)})
+    return render_template('prompt_edit.html', prompt=_p)
